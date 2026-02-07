170982d7c2bb29be7e882e98293d915feb66ddba
nepalsingh9528-star <nepalsingh9528@gmail.com>
2026-02-07 08:59:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Add 20 falling roses
+for(let i=0; i<20; i++){
+    let rose = document.createElement('div');
+    rose.className = 'rose';
+    rose.innerText = '🌹';
+    rose.style.left = Math.random()*100 + 'vw';
+    rose.style.animationDuration = 3 + Math.random()*5 + 's';
+    document.body.appendChild(rose);
+}
 
